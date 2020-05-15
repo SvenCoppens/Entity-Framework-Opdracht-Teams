bf2191c6d4c11cfd7dc9a7f6a69427fad8dd949d
Sven Coppens <sven.coppens@student.hogent.be>
2020-05-15 12:11:19 +0200

    test3

--- TeamsLibrary/Transfer.cs
+++ TeamsLibrary/Transfer.cs
@@ -7,6 +7,12 @@ namespace TeamsLibrary
 {
     public class Transfer
     {
+        //public Transfer(int spelerID,int nieuwTeamStamNummer,int oudTeamStamNummer)
+        //{
+        //    SpelerID = spelerID;
+        //    NieuwTeamStamNummer = nieuwTeamStamNummer;
+        //    OudTeamStamNummer = oudTeamStamNummer;
+        //}
         public Transfer(Speler speler,Team nieuwTeam,Team oudTeam)
         {
             Speler = speler;
@@ -14,10 +20,13 @@ namespace TeamsLibrary
             OudTeam = oudTeam;
         }
         public int TransferID { get; set; }
+        //public int SpelerID { get; set; }
         public Speler Speler { get; set; }
         public int TransferPrijs { get; set; }
         [Required]
+        //public int NieuwTeamStamNummer { get; set; }
         public Team NieuwTeam { get; set; }
+        //public int OudTeamStamNummer { get; set; }
         public Team OudTeam { get; set; }
     }
 }
